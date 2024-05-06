6ebeaef115016bf40a967893b6c0254488d5f7c2
ref <56499429+referefref@users.noreply.github.com>
2024-05-06 14:44:54 +0800

    Update DNP3pot.py

--- DNP3pot.py
+++ DNP3pot.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 # DNP3pot - The Industrial Honeypot 
 #
@@ -20,10 +20,10 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 """
-DNP3 Lib based on DNP3Lib.py [https://github.com/nrodofile/ScapyDNP3_lib]
+DNP3 Lib based on DNP3Lib.py and ported to Python3 by referefref [https://github.com/nrodofile/ScapyDNP3_lib]
 Original author: Nicholas Rodofile
+Updated by: James Brine
 """
-
 
 import signal
 import sys
@@ -33,128 +33,103 @@ import select
 import socket
 import os
 import chardet
-import thread
+import threading
 import multiprocessing
-from scapy.all import * 
-import DNP3_Lib         
+from scapy.all import *
+import DNP3_Lib
 import logging
 
-def listen(Ip,DNP3port):
-   
+def listen(Ip, DNP3port):
     p.start()
     while 1:
-
-	try:
-	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 3) # this puts socket in time_wait state make server avaiable for starvatioins and itteration attacks
-            s.bind((Ip,DNP3port))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 3)
+            s.bind((Ip, DNP3port))
             s.listen(5)
-
         except socket.error:
-	    exit(1)
+            exit(1)
 
         conn, addr = s.accept()
-	
         logger.info('New connection from {}'.format(addr))
         print('Received connection from {}'.format(addr))
-	
+
         try:
-
-	    thread.start_new_thread(new,(conn,addr))
-
- 	except:
-	    print('Terrible Error!')
-	    import traceback
-	    traceback.print_exc()
-	    print("cannot bind port :(")
+            threading.Thread(target=new, args=(conn, addr)).start()
+        except:
+            print('Terrible Error!')
+            import traceback
+            traceback.print_exc()
+            print("cannot bind port :(")
             exit(1)
-	
-def scapy():
-    sniff(filter="host "+Ip+" and tcp port "+str(DNP3port)+" and greater 50", prn=packet_callback, store=0)
 
-#Packet Inspection 
-def new(conn,addr):
-    
+def scapy(Ip, DNP3port):
+    sniff(filter="host " + Ip + " and tcp port " + str(DNP3port) + " and greater 50", prn=packet_callback, store=0)
+
+# Packet Inspection
+def new(conn, addr):
     msg = conn.recv(2048)
-    
+
     try:
-	conn.send(banner)
-	time.sleep(1.02)
-	conn.close()
+        conn.send(banner)
+        time.sleep(1.02)
+        conn.close()
     except TypeError:
-	conn.close()
+        conn.close()
 
     if not len(str(msg)) == 0:
-	encoding = chardet.detect(msg)['encoding']
-        #print('Raw data recieved from {} rawdata: {} encoding: {}'.format(addr,msg,encoding))
-	logger.info('Raw data recieved from {} rawdata: {} encoding: {}'.format(addr,msg,encoding))
+        encoding = chardet.detect(msg)['encoding']
+        logger.info('Raw data received from {} rawdata: {} encoding: {}'.format(addr, msg, encoding))
+        # TODO FullPacket
+    else:
+        conn.close()
+        return
 
-	#TODO FullPacket
-    
-    else: 
-	#time.sleep(0.52)
-	conn.close()
-	return
- 
-    
-
-
-#DNP3 SCAPY FILTER 
-	
+# DNP3 SCAPY FILTER
 def packet_callback(pkt):
-        
     if pkt.haslayer(IP) and pkt[IP].dport == DNP3port and pkt[IP].dst == Ip and pkt.haslayer(DNP3):
-	 
-	if pkt[TCP].payload:
-	    
-	    p = str(pkt.summary())  #pkt.show() #Debug pkt[RAW].load
-	    
-	else:
-	    p = 'No payload'
-	    logger.info('DNP3 application layer detected - No Payload src {} dport {} dst {} sval {} chcval {} DFCval {}'.format(pkt[IP].src, pkt[IP].dport, pkt[IP].dst,pkt.START,pkt.LENGTH,pkt.CONTROL.FCV))
-	    #print('DNP3 application layer detected - No Payload src {} dport {} dst {} sval {} chcval {} DFCval {}'.format(pkt[IP].src, pkt[IP].dport, pkt[IP].dst,pkt.START,pkt.LENGTH,pkt.CONTROL.FCV))
+        if pkt[TCP].payload:
+            p = str(pkt.summary())
+        else:
+            p = 'No payload'
+            logger.info('DNP3 application layer detected - No Payload src {} dport {} dst {} sval {} chcval {}'.format(pkt[IP].src, pkt[IP].dport, pkt[IP].dst, pkt.START, pkt.LENGTH))
+        if not p == 'No payload':
+            logger.info('DNP3 application layer detected - Payload: {} src {} dport {} dst {} sval {} chcval {}'.format(p, pkt[IP].src, pkt[IP].dport, pkt[IP].dst, pkt.START, pkt.LENGTH))
 
-	if not p == 'No payload': 
-            logger.info('DNP3 application layer detected - Payload: {} src {} dport {} dst {} sval {} chcval {}'.format(p, pkt[IP].src, pkt[IP].dport, pkt[IP].dst,pkt.START,pkt.LENGTH))
-            #print('DNP3 application layer detected - Payload:{} src {} dport {} dst {} sval {} chcval {} DFCval {}'.format(p, pkt[IP].src, pkt[IP].dport, pkt[IP].dst,pkt.START,pkt.LENGTH,pkt.CONTROL.FCV))
-                     
-	              
-        ''' Pseudo Transport Layer Check '''
-
+        # Pseudo Transport Layer Check
         if pkt.haslayer(DNP3_Lib.DNP3Transport):
-	    #print('DNP3 transport layer detected src {} dport {} dst {} FIN BIT {} FIR BIT {}'.format(pkt[IP].src, pkt[IP].dport, pkt[IP].dst,pkt.FIN,pkt.FIR))
-	    logger.info('DNP3 transport layer detected src {} dport {} dst {} FIN BIT {} FIR BIT {}'.format(pkt[IP].src, pkt[IP].dport, pkt[IP].dst,pkt.FIN,pkt.FIR))	
+            logger.info('DNP3 transport layer detected src {} dport {} dst {} FIN BIT {} FIR BIT {}'.format(pkt[IP].src, pkt[IP].dport, pkt[IP].dst, pkt.FIN, pkt.FIR))
 
-        ''' Function codes check '''
-
-        if (pkt.haslayer(DNP3_Lib.DNP3ApplicationRequest)):
-
+        # Function codes check
+        if pkt.haslayer(DNP3_Lib.DNP3ApplicationRequest):
             if pkt.FUNC_CODE is not None:
-                if (pkt.FUNC_CODE >= 0 and pkt.FUNC_CODE <= 33):
+                if 0 <= pkt.FUNC_CODE <= 33:
                     logger.critical('\n DNP3 Function code {} detected {} ----{}----> {}:\n'.format(pkt.FUNC_CODE, pkt[IP].src, pkt[IP].dport, pkt[IP].dst))
-		    #print('detected function code0: '+ str(pkt.FUNC_CODE))
-		    send(response)
+                    send(response)
 
-		if (pkt.FUNC_CODE > 33 and pkt.FUNC_CODE < 129):
+                if 33 < pkt.FUNC_CODE < 129:
                     logger.critical('\n DNP3 OUT OF RANGE Function code {} detected {} ----{}----> {}:\n'.format(pkt.FUNC_CODE, pkt[IP].src, pkt[IP].dport, pkt[IP].dst))
-		    #print('detected function code1: '+ str(pkt.FUNC_CODE))
 
-		if (pkt.FUNC_CODE >= 129 and pkt.FUNC_CODE <= 131):
+                if 129 <= pkt.FUNC_CODE <= 131:
                     logger.critical('\n DNP3 Function code {} detected {} ----{}----> {}:\n'.format(pkt.FUNC_CODE, pkt[IP].src, pkt[IP].dport, pkt[IP].dst))
-		    #print('detected function code2: '+ str(pkt.FUNC_CODE))
-        	    send(response)
-	
-                if (pkt.FUNC_CODE < 0 and pkt.FUNC_CODE > 131):
+                    send(response)
+
+                if not 0 <= pkt.FUNC_CODE <= 131:
                     logger.critical('\n DNP3 OUT OF RANGE Function code {} detected {} ----{}----> {}:\n'.format(pkt.FUNC_CODE, pkt[IP].src, pkt[IP].dport, pkt[IP].dst))
-		    #print('detected function code3: '+ str(pkt.FUNC_CODE))
-               
+
+def get_eth0_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+        return ip_address
+    except socket.error:
+        return None
+
 if __name__ == "__main__":
-
-    print('Initialised')
-    global s
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Super smart way to go around the most annoying globa error eva!
-    # Logging configuration
-    global logger
+    print('Initialized')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     logger = logging.getLogger(__name__)
     logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
     logger.setLevel(logging.INFO)
@@ -163,31 +138,29 @@ if __name__ == "__main__":
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s\n')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    global p
-    p = multiprocessing.Process(target=scapy)
-    global DNP3
-    DNP3 = DNP3_Lib.DNP3 #Reference to DNP3 class defined in DNP3_Lib
+    DNP3 = DNP3_Lib.DNP3
     DNP3port = 20000
-    global Ip    
-    Ip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]  
-    bind_layers(TCP, DNP3, dport = DNP3port)
-    bind_layers(TCP, DNP3, sport = DNP3port)
-    
-    global response	
-    global banner
-    banner = '\x05\x64\x0A\x00\x01\x00\x05\x00\x39\x71\xC0\xF4\x82\x00\x00\x6F\xBA' 
-    
-    response = IP()/TCP()/'\x05\x64\x0A\x00\x01\x00\x05\x00\x39\x71\xC0\xF4\x82\x00\x00\x6F\xBA'
-    
 
+	# Remove original ip command exec and replace with python sockets implementation instead for better portability
+    Ip = get_eth0_ip()
+    if Ip:
+        print("Eth0 IP address:", Ip)
+    else:
+        print("Failed to retrieve eth0 IP address. Exiting.")
+        sys.exit(1)
+    
+    bind_layers(TCP, DNP3, dport=DNP3port)
+    bind_layers(TCP, DNP3, sport=DNP3port)
+
+    response = IP() / TCP() / '\x05\x64\x0A\x00\x01\x00\x05\x00\x39\x71\xC0\xF4\x82\x00\x00\x6F\xBA'
+    banner = '\x05\x64\x0A\x00\x01\x00\x05\x00\x39\x71\xC0\xF4\x82\x00\x00\x6F\xBA'
+
+    p = multiprocessing.Process(target=scapy, args=(Ip, DNP3port))
+    
     try:
-        listen(Ip,DNP3port)
+        listen(Ip, DNP3port)
     except KeyboardInterrupt:
         print('\nInterrupted')
-	s.close()
-	p.join()
+        s.close()
+        p.join()
         sys.exit(0)
-
-        
-
-        
